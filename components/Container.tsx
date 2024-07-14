41b25205b4ae6c811c4ec34f30f93c3f983fb0ad
Marcin Niedbalec <marcin.niedbalec@proton.me>
2024-07-14 18:33:12 +0200

    fix: remove unnecessary code

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,6 +1,4 @@
-import { View } from "react-native";
 import React, { FC, ReactNode } from "react";
-import { useAppTheme } from "@/lib/theme/Material3ThemeProvider";
 import { ThemedView } from "./ThemedView";
 
 type Props = {
@@ -8,14 +6,10 @@ type Props = {
   withHeader: boolean;
 };
 const Container: FC<Props> = ({ children, withHeader = false }) => {
-  const theme = useAppTheme();
-  console.log("🚀 ~ container:", theme.colors.background);
-
   return (
     <ThemedView
       style={{
         flex: 1,
-        // backgroundColor: theme.colors.background,
         paddingTop: withHeader ? 18 : 98,
         paddingLeft: 18,
         paddingRight: 18,
