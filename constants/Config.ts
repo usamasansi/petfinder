a3d896c08815ff51b685340559d1dbb4c3070a65
Marcin Niedbalec <marcin.niedbalec@proton.me>
2024-07-21 15:57:47 +0200

    fix: connecting to localhost server in android

--- constants/Config.ts
+++ constants/Config.ts
@@ -1,3 +1,8 @@
 // todo: load config from env
 
-export const API_BASE_URL = "http://localhost:3333/api";
+import { Platform } from "react-native";
+
+// if you don't run your app in expo go, but in generated APK in Android -> change localhost to: your pc ip or 10.0.2.2
+export const API_BASE_URL = `http://${
+  Platform.OS === "android" ? "10.0.2.2" : "localhost"
+}:3333/api`;
