e2b1ff7e120c2081fb72f926b2a154d1d360fbc0
Marcin Niedbalec <marcin.niedbalec@proton.me>
2024-07-21 15:58:05 +0200

    feat: explore header

--- components/ExploreHeader.tsx
+++ components/ExploreHeader.tsx
@@ -1,17 +1,53 @@
-import { StyleSheet, TouchableOpacity, View } from "react-native";
-import React from "react";
+import {
+  Platform,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useRef, useState } from "react";
 import { ThemedView } from "./ThemedView";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Icon, IconButton, Text } from "react-native-paper";
 import { Link } from "expo-router";
 import { useAppTheme } from "@/lib/theme/Material3ThemeProvider";
 import { Ionicons } from "@expo/vector-icons";
+import * as Haptics from "expo-haptics";
+import { StatusBar } from "expo-status-bar";
+import { useHeaderHeight } from "@react-navigation/elements";
+
+const categories = [
+  {
+    id: 1,
+    name: "Dog",
+    icon: "dog",
+  },
+  {
+    id: 2,
+    name: "Cat",
+    icon: "cat",
+  },
+  {
+    id: 3,
+    name: "Other",
+    icon: "ab-testing",
+  },
+];
 
 export function ExploreHeader() {
   const theme = useAppTheme();
+  const itemsRef = useRef<Array<TouchableOpacity | null>>([]);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const selectCategory = (index: number) => {
+    setActiveIndex(index);
+  };
+
+  const statusBarHeight = useHeaderHeight();
+  console.log("🚀 ~ ExploreHeader ~ statusBarHeight:", statusBarHeight);
   return (
     <ThemedView>
-      <SafeAreaView>
+      <SafeAreaView style={{ paddingTop: Platform.OS === "android" ? 28 : 0 }}>
         <View style={styles.container}>
           <View style={styles.actionRow}>
             <Link
@@ -45,6 +81,53 @@ export function ExploreHeader() {
               onPress={() => console.log("Pressed")}
             />
           </View>
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{
+              alignItems: "center",
+              gap: 20,
+              paddingHorizontal: 16,
+            }}
+          >
+            {categories.map((item, index) => {
+              return (
+                <TouchableOpacity
+                  key={index}
+                  ref={(element) => (itemsRef.current[index] = element)}
+                  onPress={() => selectCategory(index)}
+                  style={
+                    activeIndex === index
+                      ? {
+                          ...styles.categoriesBtnActive,
+                          borderBottomColor: theme.colors.primary,
+                        }
+                      : styles.categoriesBtn
+                  }
+                >
+                  <Icon
+                    source={item.icon}
+                    size={24}
+                    color={
+                      activeIndex === index ? theme.colors.primary : undefined
+                    }
+                  />
+                  <Text
+                    variant="labelLarge"
+                    style={{
+                      color:
+                        activeIndex === index
+                          ? theme.colors.primary
+                          : theme.colors.onSurface,
+                      fontWeight: "600",
+                    }}
+                  >
+                    {item.name}
+                  </Text>
+                </TouchableOpacity>
+              );
+            })}
+          </ScrollView>
         </View>
       </SafeAreaView>
     </ThemedView>
@@ -77,5 +160,18 @@ const styles = StyleSheet.create({
       width: 1,
       height: 1,
     },
+  },
+  categoriesBtn: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: 8,
+  },
+  categoriesBtnActive: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: 8,
+    borderBottomWidth: 2,
   },
 });
