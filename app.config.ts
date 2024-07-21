44e2e751739dca6c3fc22012f76fe21ac3d4a8c2
Marcin Niedbalec <marcin.niedbalec@proton.me>
2024-07-21 15:08:00 +0200

    feat: add app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,38 @@
+import { ExpoConfig, ConfigContext } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "lost-pet-finder-app",
+  slug: "lost-pet-finder-app",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "myapp",
+  userInterfaceStyle: "automatic",
+  splash: {
+    image: "./assets/images/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.belkocik.lostpetfinder",
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+    package: "com.belkocik.lostpetfinder",
+    softwareKeyboardLayoutMode: "pan",
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: ["expo-router", "expo-localization", "expo-secure-store"],
+  experiments: {
+    typedRoutes: true,
+  },
+});
